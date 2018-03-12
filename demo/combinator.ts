fec63ef9dd9ba34c475f6fdd087943f8320c75c8
gcanti <giulio.canti@gmail.com>
2018-03-12 10:48:33 +0100

    Demo: più commenti a combinator.ts

--- demo/combinator.ts
+++ demo/combinator.ts
@@ -66,7 +66,9 @@ quux;`
 
 const toContactInfos2 = (
   csv: string,
+  // se `true` elimina le righe senza name
   nameRequired: boolean,
+  // se `true` elimina le righe senza email
   emailRequired: boolean
 ): Array<ContactInfo> => {
   return csv
@@ -98,39 +100,34 @@ const toContactInfos2 = (
 
 /*
 
-  Separiamo la logica di parsing da quella di mapping
+  Separiamo la logica di parsing da quella di filtering e mapping
 
 */
 
 type Pair<A> = [A, A]
-
-// const getTokens1 = (csv: string): Array<Pair<string>> =>
-//   csv.split('\n').map(line => line.split(';')) // Type 'string[][]' is not assignable to type '[string, string][]'
-
-/*
-
-  Il type system ci aiuta a non dimenticare casi non gestiti
-
-*/
 
 const toPair = (xs: Array<string>): Pair<string> => [
   xs.length > 0 ? xs[0].trim() : '',
   xs.length > 1 ? xs[1].trim() : ''
 ]
 
+// parsing
 const getTokens = (csv: string): Array<Pair<string>> =>
   csv.split('\n').map(line => toPair(line.split(';')))
 
-const toContactInfo = (
-  [name, email]: Pair<string>
-): ContactInfo => ({ name, email })
+// mapping
+const toContactInfo = ([name, email]: Pair<
+  string
+>): ContactInfo => ({ name, email })
 
 const toContactInfos3 = (
   csv: string,
   nameRequired: boolean,
   emailRequired: boolean
 ): Array<ContactInfo> =>
+  // parsing
   getTokens(csv)
+    // filtering
     .filter(([name, email]) => {
       if (
         (name === '' && nameRequired) ||
@@ -140,18 +137,24 @@ const toContactInfos3 = (
       }
       return true
     })
+    // mapping
     .map(toContactInfo)
 
 /*
 
-    Separiamo anche la logica di filtro
+    Separiamo anche la logica di filtering
 
 */
 
+// Un "predicato" su `A` è una funzione che accetta in input un valore
+// di tipo `A` e restituisce un booleano
 type Predicate<A> = (a: A) => boolean
 
+// Possiamo ora rendere formale il concetto di "filtro" per le righe del CSV
+// Definizione: chiamiamo "filtro" un predicato su `Pair<string>`
 type Filter = Predicate<Pair<string>>
 
+// filtering
 const getFilter = (
   nameRequired: boolean,
   emailRequired: boolean
@@ -176,6 +179,7 @@ const toContactInfos4 = (
 
 /*
 
+  Fino ad ora ho fatto un refactoring che non ha modificato in alcun modo la funzionlità
   Ma adesso è evidente che i parametri aggiuntivi servono solo per creare il predicato.
   Ma allora passiamo direttamente il predicato come argomento!
 
