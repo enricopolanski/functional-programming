253b92280df5acf5d5dd5fca304dc0a0ad9bc11a
gcanti <giulio.canti@gmail.com>
2021-01-29 11:28:20 +0100

    ADT01

--- src/solutions/ADT01.ts
+++ src/solutions/ADT01.ts
@@ -1,123 +1,40 @@
 /**
- * Modellare il punteggio di un game di tennis
+ * Modellare un albero binario completo, il/i costruttore/i, la funzione di pattern matching
+ * e una funzione che converte l'albero in un `ReadonlyArray`
  */
+export type BinaryTree<A> =
+  | { readonly _tag: 'Leaf'; readonly value: A }
+  | {
+      readonly _tag: 'Node'
+      readonly value: A
+      readonly left: BinaryTree<A>
+      readonly right: BinaryTree<A>
+    }
 
-// ------------------------------------
-// model
-// ------------------------------------
+export const leaf = <A>(value: A): BinaryTree<A> => ({ _tag: 'Leaf', value })
 
-type Player = 'A' | 'B'
+export const node = <A>(
+  value: A,
+  left: BinaryTree<A>,
+  right: BinaryTree<A>
+): BinaryTree<A> => ({ _tag: 'Node', value, left, right })
 
-type Score = 0 | 15 | 30 | 40
-
-type Game =
-  | { readonly _tag: 'Score'; readonly A: Score; readonly B: Score }
-  | { readonly _tag: 'Advantage'; readonly player: Player }
-  | { readonly _tag: 'Deuce' }
-  | { readonly _tag: 'Game'; readonly player: Player }
-
-// ------------------------------------
-// constructors
-// ------------------------------------
-
-const score = (A: Score, B: Score): Game => ({ _tag: 'Score', A, B })
-const advantage = (player: Player): Game => ({ _tag: 'Advantage', player })
-const deuce: Game = { _tag: 'Deuce' }
-const game = (player: Player): Game => ({ _tag: 'Game', player })
-
-/**
- * Punteggio di partenza
- */
-const start: Game = {
-  _tag: 'Score',
-  A: 0,
-  B: 0
-}
-
-// ------------------------------------
-// destructors
-// ------------------------------------
-
-const fold = <R>(
-  onScore: (scoreA: Score, scoreB: Score) => R,
-  onAdvantage: (player: Player) => R,
-  onDeuce: () => R,
-  onGame: (player: Player) => R
-) => (game: Game): R => {
-  switch (game._tag) {
-    case 'Score':
-      return onScore(game.A, game.B)
-    case 'Advantage':
-      return onAdvantage(game.player)
-    case 'Deuce':
-      return onDeuce()
-    case 'Game':
-      return onGame(game.player)
+export const fold = <A, B>(
+  onLeaf: (a: A) => B,
+  onNode: (value: A, left: BinaryTree<A>, right: BinaryTree<A>) => B
+) => (tree: BinaryTree<A>): B => {
+  switch (tree._tag) {
+    case 'Leaf':
+      return onLeaf(tree.value)
+    case 'Node':
+      return onNode(tree.value, tree.left, tree.right)
   }
 }
 
-import * as O from 'fp-ts/Option'
-
-const next = (score: Score): O.Option<Score> => {
-  switch (score) {
-    case 0:
-      return O.some(15)
-    case 15:
-      return O.some(30)
-    case 30:
-      return O.some(40)
-    case 40:
-      return O.none
-  }
-}
-
-/**
- * Dato un punteggio e un giocatore che si è aggiudicato il punto restituisce il nuovo punteggio
- */
-const win = (player: Player): ((game: Game) => Game) =>
-  fold(
-    (A, B) =>
-      pipe(
-        next(player === 'A' ? A : B),
-        O.fold(
-          (): Game => (A === B ? advantage(player) : game(player)),
-          (next) => (player === 'A' ? score(next, B) : score(A, next))
-        )
-      ),
-    (current) => (player === current ? game(player) : deuce),
-    () => advantage(player),
-    game
-  )
-
-/**
- * Restituisce il punteggio in formato leggibile
- */
-const show: (game: Game) => string = fold(
-  (A, B) => `${A} - ${B === A ? 'all' : B}`,
-  (player) => `advantage player ${player}`,
-  () => 'deuce',
-  (player) => `game player ${player}`
+export const toReadonlyArray: <A>(
+  tree: BinaryTree<A>
+) => ReadonlyArray<A> = fold(
+  (value) => [value],
+  (value, left, right) =>
+    [value].concat(toReadonlyArray(left)).concat(toReadonlyArray(right))
 )
-
-// ------------------------------------
-// tests
-// ------------------------------------
-
-import * as assert from 'assert'
-import { pipe } from 'fp-ts/function'
-
-assert.deepStrictEqual(
-  pipe(start, win('A'), win('A'), win('A'), win('A'), show),
-  'game player A'
-)
-
-const fifteenAll = pipe(start, win('A'), win('B'))
-assert.deepStrictEqual(pipe(fifteenAll, show), '15 - all')
-
-const fourtyAll = pipe(fifteenAll, win('A'), win('B'), win('A'), win('B'))
-assert.deepStrictEqual(pipe(fourtyAll, show), '40 - all')
-
-const advantageA = pipe(fourtyAll, win('A'))
-assert.deepStrictEqual(pipe(advantageA, show), 'advantage player A')
-
-assert.deepStrictEqual(pipe(advantageA, win('B'), show), 'deuce')
