d03846af1fd9c5263c84ad8b30404b380c513a98
gcanti <giulio.canti@gmail.com>
2021-02-25 07:02:29 +0100

    Onion Architecture

--- src/onion-architecture/6.ts
+++ src/onion-architecture/6.ts
@@ -4,158 +4,134 @@
 
 */
 
-// import * as fs from 'fs'
-// import { URI, Task, task } from './node_modules/fp-ts/Task'
-// import { array } from './node_modules/fp-ts/Array'
-// import { sequence_ } from './node_modules/fp-ts/Foldable'
-// import { URIS, Type } from 'fp-ts/HKT'
-// import { Monad1 } from './node_modules/fp-ts/Monad'
+import * as fs from 'fs'
+import * as T from 'fp-ts/Task'
+import * as RA from 'fp-ts/ReadonlyArray'
+import { URIS, Kind } from 'fp-ts/HKT'
+import { Monad1 } from 'fp-ts/Monad'
+import { Applicative1 } from 'fp-ts/Applicative'
+import { constVoid, pipe } from 'fp-ts/function'
 
-// class Employee {
-//   constructor(
-//     readonly lastName: string,
-//     readonly firstName: string,
-//     readonly dateOfBirth: Date,
-//     readonly email: string
-//   ) {}
-//   isBirthday(today: Date): boolean {
-//     return (
-//       this.dateOfBirth.getMonth() === today.getMonth() &&
-//       this.dateOfBirth.getDate() === today.getDate()
-//     )
-//   }
-// }
+class Employee {
+  constructor(
+    readonly lastName: string,
+    readonly firstName: string,
+    readonly dateOfBirth: Date,
+    readonly email: string
+  ) {}
+  isBirthday(today: Date): boolean {
+    return (
+      this.dateOfBirth.getMonth() === today.getMonth() &&
+      this.dateOfBirth.getDate() === today.getDate()
+    )
+  }
+}
 
-// class Email {
-//   constructor(
-//     readonly from: string,
-//     readonly subject: string,
-//     readonly body: string,
-//     readonly recipient: string
-//   ) {}
-// }
+class Email {
+  constructor(
+    readonly from: string,
+    readonly subject: string,
+    readonly body: string,
+    readonly recipient: string
+  ) {}
+}
 
-// //
-// // type classes
-// //
+//
+// type classes
+//
 
-// interface MonadEmail<M extends URIS> {
-//   sendMessage: (email: Email) => Type<M, void>
-// }
+interface MonadEmail1<M extends URIS> {
+  readonly sendMessage: (email: Email) => Kind<M, void>
+}
 
-// interface MonadFileSystem<M extends URIS> {
-//   read: (fileName: string) => Type<M, string>
-// }
+interface MonadFileSystem1<M extends URIS> {
+  readonly read: (fileName: string) => Kind<M, string>
+}
 
-// interface MonadApp<M extends URIS>
-//   extends MonadEmail<M>,
-//     MonadFileSystem<M>,
-//     Monad1<M> {}
+interface MonadApp<M extends URIS>
+  extends MonadEmail1<M>,
+    MonadFileSystem1<M>,
+    Monad1<M>,
+    Applicative1<M> {}
 
-// // pure
-// const toEmail = (employee: Employee): Email => {
-//   const recipient = employee.email
-//   const body = `Happy Birthday, dear ${employee.firstName}!`
-//   const subject = 'Happy Birthday!'
-//   return new Email(
-//     'sender@here.com',
-//     subject,
-//     body,
-//     recipient
-//   )
-// }
+// pure
+const toEmail = (employee: Employee): Email => {
+  const recipient = employee.email
+  const body = `Happy Birthday, dear ${employee.firstName}!`
+  const subject = 'Happy Birthday!'
+  return new Email('sender@here.com', subject, body, recipient)
+}
 
-// // pure
-// const getGreetings = (
-//   today: Date,
-//   employees: Array<Employee>
-// ): Array<Email> => {
-//   return employees
-//     .filter(e => e.isBirthday(today))
-//     .map(toEmail)
-// }
+// pure
+const getGreetings = (
+  today: Date,
+  employees: ReadonlyArray<Employee>
+): ReadonlyArray<Email> => {
+  return employees.filter((e) => e.isBirthday(today)).map(toEmail)
+}
 
-// // pure
-// const parse = (input: string): Array<Employee> => {
-//   const lines = input.split('\n').slice(1) // skip header
-//   return lines.map(line => {
-//     const employeeData = line.split(', ')
-//     return new Employee(
-//       employeeData[0],
-//       employeeData[1],
-//       new Date(employeeData[2]),
-//       employeeData[3]
-//     )
-//   })
-// }
+// pure
+const parse = (input: string): ReadonlyArray<Employee> => {
+  const lines = input.split('\n').slice(1) // skip header
+  return lines.map((line) => {
+    const employeeData = line.split(', ')
+    return new Employee(
+      employeeData[0],
+      employeeData[1],
+      new Date(employeeData[2]),
+      employeeData[3]
+    )
+  })
+}
 
-// // pure
-// const sendGreetings = <M extends URIS>(M: MonadApp<M>) => (
-//   fileName: string,
-//   today: Date
-// ): Type<M, void> => {
-//   return M.chain(
-//     M.map(M.read(fileName), input =>
-//       getGreetings(today, parse(input))
-//     ),
-//     emails => sequence_(M, array)(emails.map(M.sendMessage))
-//   )
-// }
+// pure
+const sendGreetings = <M extends URIS>(M: MonadApp<M>) => (
+  fileName: string,
+  today: Date
+): Kind<M, void> => {
+  return pipe(
+    M.read(fileName),
+    M.map((input) => getGreetings(today, parse(input))),
+    M.chain(RA.traverse(M)(M.sendMessage)),
+    M.map(constVoid)
+  )
+}
 
-// //
-// // instances
-// //
+//
+// instances
+//
 
-// const getMonadApp = (
-//   smtpHost: string,
-//   smtpPort: number
-// ): MonadApp<URI> => {
-//   return {
-//     ...task,
-//     sendMessage: email =>
-//       new Task(
-//         () =>
-//           new Promise(resolve => {
-//             console.log('sending email...')
-//             setTimeout(
-//               () =>
-//                 resolve(
-//                   console.log(smtpHost, smtpPort, email)
-//                 ),
-//               1000
-//             )
-//           })
-//       ),
-//     read: fileName =>
-//       new Task(
-//         () =>
-//           new Promise(resolve => {
-//             console.log('reading file...')
-//             setTimeout(
-//               () =>
-//                 fs.readFile(
-//                   fileName,
-//                   { encoding: 'utf8' },
-//                   (_, data) => resolve(data)
-//                 ),
-//               1000
-//             )
-//           })
-//       )
-//   }
-// }
+const getMonadApp = (smtpHost: string, smtpPort: number): MonadApp<T.URI> => {
+  return {
+    ...T.Monad,
+    ...T.ApplicativePar,
+    sendMessage: (email) => () =>
+      new Promise((resolve) => {
+        console.log('sending email...')
+        setTimeout(() => resolve(console.log(smtpHost, smtpPort, email)), 1000)
+      }),
+    read: (fileName) => () =>
+      new Promise((resolve) => {
+        console.log('reading file...')
+        setTimeout(
+          () =>
+            fs.readFile(fileName, { encoding: 'utf8' }, (_, data) =>
+              resolve(data)
+            ),
+          1000
+        )
+      })
+  }
+}
 
-// const program = sendGreetings(getMonadApp('localhost', 80))
-// program(
-//   'src/refactoring/employee_data.txt',
-//   new Date(2008, 9, 8)
-// ).run()
-// /*
-// reading file...
-// sending email...
-// localhost 80 Email {
-//   from: 'sender@here.com',
-//   subject: 'Happy Birthday!',
-//   body: 'Happy Birthday, dear John!',
-//   recipient: 'john.doe@foobar.com' }
-// */
+const program = sendGreetings(getMonadApp('localhost', 80))
+program('src/onion-architecture/employee_data.txt', new Date(2008, 9, 8))()
+/*
+reading file...
+sending email...
+localhost 80 Email {
+  from: 'sender@here.com',
+  subject: 'Happy Birthday!',
+  body: 'Happy Birthday, dear John!',
+  recipient: 'john.doe@foobar.com' }
+*/
