418e54f39aba704b9cb0abadfda2400c8f20fe86
YiCChi <syanxk@gmail.com>
2023-08-01 23:37:59 +0900

    build: bump deps

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+  },
+};
